7c182a5544c80f191790edc04e194950b728ddd0
Ruffobh <francoruffino.90@gmail.com>
2026-01-28 08:56:19 -0300

    Fix generated Supabase helper types for build

--- types/supabase.ts
+++ types/supabase.ts
@@ -141,25 +141,25 @@ export type Tables<
     | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
     TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    ? keyof ((Database[PublicTableNameOrOptions["schema"]] as any)["Tables"] &
+    (Database[PublicTableNameOrOptions["schema"]] as any)["Views"])
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-            Row: infer R
-        }
-    ? R
-    : never
-    : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
+    ?(Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+    Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+    Row: infer R
+}
+    ?R
+: never
+: PublicTableNameOrOptions extends keyof(PublicSchema["Tables"] &
+    PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-            Row: infer R
-        }
-    ? R
-    : never
-    : never
+    Row: infer R
+}
+    ?R
+: never
+: never
 
 export type TablesInsert<
     PublicTableNameOrOptions extends
