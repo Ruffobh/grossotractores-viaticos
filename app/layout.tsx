10f9be6741b77c88bc0e570408cab4affcd67af9
Ruffobh <francoruffino.90@gmail.com>
2026-01-29 11:19:48 -0300

    fix: delete old favicon and force use of new icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Grosso Viaticos",
   description: "Expense management for Grosso Tractores",
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 export default function RootLayout({
