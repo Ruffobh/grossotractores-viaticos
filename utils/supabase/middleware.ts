67c27e26066a6164d13fd318c891f21a59eddadd
Ruffobh <francoruffino.90@gmail.com>
2026-02-23 15:08:42 -0300

    chore: remove temporary omitted resend api and whitelist

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -50,9 +50,7 @@ export async function updateSession(request: NextRequest) {
         if (
             !user &&
             !request.nextUrl.pathname.startsWith('/login') &&
-            !request.nextUrl.pathname.startsWith('/auth') &&
-            !request.nextUrl.pathname.startsWith('/api/resend') &&
-            !request.nextUrl.pathname.startsWith('/api/resend-omitted')
+            !request.nextUrl.pathname.startsWith('/auth')
         ) {
             console.log(`[Middleware] Redirecting to /login`);
             // no user, potentially respond by redirecting the user to the login page
@@ -71,9 +69,7 @@ export async function updateSession(request: NextRequest) {
 
         if (
             !request.nextUrl.pathname.startsWith('/login') &&
-            !request.nextUrl.pathname.startsWith('/auth') &&
-            !request.nextUrl.pathname.startsWith('/api/resend') &&
-            !request.nextUrl.pathname.startsWith('/api/resend-omitted')
+            !request.nextUrl.pathname.startsWith('/auth')
         ) {
             const url = request.nextUrl.clone()
             url.pathname = '/login'
